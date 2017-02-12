7c038f401ae7415c71dd8a67949406a8d4d71803
Shayna Thompson <sthompson@belizetelemedia.net>
2017-02-12 11:14:51 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express'); //web application frrame work aka ExpressJS
+
+
+
+var app = express();
+
+app.get('/', function(req, res)
+{
+	
+	res.sendFile(__dirname+'/index.html');
+});
+
+
+
+app.post('/login', function(req, res){
+	res.end("Login Successful.");
+	
+	
+});
+
+app.get('/queryProducts', function(req, res){
+	
+	res.json([{name: 'iphone7'}, {name: 'iPad mini'}]);
+});
+
+app.listen(3000,function()
+{
+	console.log("Server runnin on port 3000"); /*http://localhost:3000*/
+	
+});
+
+console.log("a quote a day project started")
